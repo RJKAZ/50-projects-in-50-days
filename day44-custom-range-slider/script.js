41cc483af20d578d122a6da6ce0b9609dadfcefe
Richard Kazanowski <rjkaz@comcast.net>
2021-02-26 18:28:31 -0500

    update

--- day44-custom-range-slider/script.js
+++ day44-custom-range-slider/script.js
@@ -6,14 +6,29 @@ range.addEventListener('input', (e) => {
 
   const range_width = getComputedStyle(e.target).
   getPropertyValue('width')
-  const label_width = getComputedStyle(e.label).
+  const label_width = getComputedStyle(label).
   getPropertyValue('width')
 
-  const num_width = +range_width.substring(0, range_width.length -2)
+  const num_width = +range_width.substring(0, 
+    range_width.length -2)
 
-  console.log(range_width, label_width)
+    const num_label_width = +label_width.substring(0, 
+      label_width.length -2)
+
+  const max = +e.target.max
+  const min = +e.target.min
+
+  const left = value * (num_width / max) - 
+  num_label_width / 2 + scale(value, min, max, 10, -10)
+
+  label.style.left = `${left}px`
 
 
   label.innerHTML = value 
 
 })
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
