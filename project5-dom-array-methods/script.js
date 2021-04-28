032afa9b18bf36c2300b0260308d7660d4d612c0
Richard Kazanowski <rjkaz@comcast.net>
2021-04-28 16:47:09 -0400

    error

--- project5-dom-array-methods/script.js
+++ project5-dom-array-methods/script.js
@@ -33,4 +33,29 @@ async function getRandomUser() {
 
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+// update DOM
+function updateDOM(providedData = data) {
+  //clear the main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+    main.appendChild(element);
+  });
+
+}
+
+// format number as money
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+
+}
+
+// event listener
+addUserBtn.addEventListener('click', getRandomUser);
