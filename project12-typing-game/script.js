8a3d463b407ce113c5535e4a6c263df73c099975
Reese RJ Kaz <rjkaz@comcast.net>
2021-05-31 17:11:59 -0400

    project 12 finished

--- project12-typing-game/script.js
+++ project12-typing-game/script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
@@ -11,67 +11,128 @@ const difficultySelect = document.getElementById('difficulty');
 // list of words for game
 
 const words = [
-    'sigh',
-    'tense',
-    'airplane',
-    'ball',
-    'pies',
-    'juice',
-    'warlike',
-    'bad',
-    'north',
-    'dependent',
-    'steer',
-    'silver',
-    'highfalutin',
-    'superficial',
-    'quince',
-    'eight',
-    'feeble',
-    'admit',
-    'drag',
-    'loving'
+  'sigh',
+  'tense',
+  'airplane',
+  'ball',
+  'pies',
+  'juice',
+  'warlike',
+  'bad',
+  'north',
+  'dependent',
+  'steer',
+  'silver',
+  'highfalutin',
+  'superficial',
+  'quince',
+  'eight',
+  'feeble',
+  'admit',
+  'drag',
+  'loving',
 ];
 
 // init word
 let randomWord;
 
-// init score 
+// init score
 let score = 0;
 
 //init time
 let time = 10;
 
+// set difficulty to value in ls or medium
+let difficulty =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
+
+// set difficulty select value
+difficultySelect.value =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
+
+// Focus on text on start
+text.focus();
+
+// Start Counting down
+const timeInterval = setInterval(updateTime, 1000);
+
 // Generate random word from array
 function getRandomWord() {
-    return words [Math.floor(Math.random() * words.length)];
+  return words[Math.floor(Math.random() * words.length)];
 }
 
 // Add word to DOM
 function addWordToDom() {
-    randomWord = getRandomWord();
-    word.innerHTML = randomWord;
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
 }
 
 // Update Score
 function updateScore() {
-    score++;
-    scoreEl.innerHTML = score;
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+// Update Time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    // end game
+    gameOver();
+  }
+}
+
+// Game Over, show end screen
+
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+    `;
+
+  endgameEl.style.display = 'flex';
 }
 
 addWordToDom();
 
 // event listeners
 
-text.addEventListener('input', e => {
-    const insertedText = e.target.value;
-    
-    if(insertedText === randomWord) {
-        addWordToDom();
-        updateScore();
+// Typing
+text.addEventListener('input', (e) => {
+  const insertedText = e.target.value;
 
-        // Clear
-        e.target.value = '';
+  if (insertedText === randomWord) {
+    addWordToDom();
+    updateScore();
+
+    // Clear
+    e.target.value = '';
+
+    if (difficulty === 'hard') {
+      time += 2;
+    } else if (difficulty === 'medium') {
+      time += 3;
+    } else {
+      time += 5;
     }
 
+    updateTime();
+  }
+});
+
+// Settings btn click
+settingBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+// Settings select
+settingsForm.addEventListener('change', (e) => {
+  difficulty = e.target.value;
+  localStorage.setItem('difficulty', difficulty);
 });
