e6f68e5a9ce249a99f4775a9c49d3437affb1a00
unknown <rjkaz@comcast.net>
2021-06-16 21:43:18 -0800

    project 15 finished, but it does't work, API issues

--- project15-lyrics-search/script.js
+++ project15-lyrics-search/script.js
@@ -72,6 +72,22 @@ async function getMoreSongs(url) {
 
 }
 
+//get lyrcis for song
+
+async function getLyrics(artists, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>`;
+
+    more.innerHTML = '';
+    
+
+}
+
 
 //event listeners
 form.addEventListener('submit', e => {
@@ -86,3 +102,14 @@ form.addEventListener('submit', e => {
     }
 
 });
+
+// Get Lyrics button click
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+
+    if(clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
+        getLyrics(artist, songTitle);
+    }
+})
