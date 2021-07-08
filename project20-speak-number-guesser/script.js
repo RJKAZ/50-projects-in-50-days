f3c77cf4bcf0a8d5dfe8f3413f78425dff0ba5d4
reese <rjkaz@comcast.net>
2021-07-08 19:49:36 -0400

    project 20 finished

--- project20-speak-number-guesser/script.js
+++ project20-speak-number-guesser/script.js
@@ -1,8 +1,8 @@
 const msgEl = document.getElementById('msg');
 
-const randonNum = getRandomNumber();
+const randomNum = getRandomNumber();
 
-console.log('Number:', randonNum);
+console.log('Number:', randomNum);
 
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -16,8 +16,44 @@ recognition.start();
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
 
-  // writeMessage(msg);
-  // checkNumber(msg);
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+// Write what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+  <div>You said: </div>
+  <span class="box">${msg}</span>
+  `;
+}
+
+// check msg against number
+function checkNumber(msg) {
+  const num = +msg;
+
+  //check if valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += '<div>That is not a valid number</div>';
+    return;
+  }
+  //check if in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML = '<div>Number must be between 1 and 100</div>';
+    return;
+  }
+  //check number
+  if (num === randonNum) {
+    document.body.innerHTML = `
+    <h2>Congrats! You have guessed the number! <br><br>
+    It was ${num}</h2>
+    <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += '<div>GO LOWER</div>';
+  } else {
+    msgEl.innerHTML += '<div>GO HIGHER</div>';
+  }
 }
 
 // Generate Random Number
@@ -29,3 +65,12 @@ function getRandomNumber() {
 // speak result
 
 recognition.addEventListener('result', onSpeak);
+
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
+
+document.body.addEventListener('click', (e) => {
+  if (e.target.id == 'play-again') {
+    window.location.reload();
+  }
+});
