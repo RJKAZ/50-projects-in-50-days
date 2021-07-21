7fee7bff09791e5c61a29f8ac42ffe754a8ae7b2
RJ <rjkaz@comcast.net>
2021-07-21 19:59:45 -0400

    30days day1

---
+++ 30-days-of-js/exercise-1/variable.js
@@ -0,0 +1,27 @@
+var variable1 = "I love New York City " // String
+var variable2 = 10000 // number
+var variable3 = true // boolean
+var variable4  // undefined (give it no value)
+var variable5 = null 
+
+
+console.log(typeof variable1);
+console.log(typeof variable2);
+console.log(typeof variable3);
+console.log(typeof variable4);
+console.log(typeof variable5);
+
+
+var firstName = "RJ";
+var lastName = "Kaz";
+var maritalStatus = "Single";
+var country = "USA";
+var age = 37;
+
+var me = ["RJ", "Kaz", "Single", "USA", 37];
+
+var myAge = 37;
+var yourAge = 100;
+
+console.log(myAge);
+console.log(yourAge);
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Another Udemy Course to sharpen my CSS & Javascript Skills
 
 The intial course was simply 50 quick projects over 50 days, which I completed. However the same instructor had an addtional course for 20 Web Projects with Vanillia JavaScript. Its kind of almost a continuation of the 50 projects, but the addtional 20 are longer. So I will be putting them in the same repo. 
+
+
+Now adding another 30-day challenge 
+https://github.com/Asabeneh/30-Days-Of-JavaScript
