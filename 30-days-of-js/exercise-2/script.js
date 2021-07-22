d26b833588e254ad4d8b8eb0f2a1b0d8f7adad74
Richard Kazanowski <rjkaz@comcast.net>
2021-07-22 15:32:02 -0400

    exercise 2 - still working on

---
+++ 30-days-of-js/exercise-2/script.js
@@ -0,0 +1,13 @@
+let word = "javascript"
+
+word = "phyton";
+
+console.log(word);
+
+let challenge = "30 days of JavaScript";
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substr(3));
+// Left off on exercise Level 1 - 7
