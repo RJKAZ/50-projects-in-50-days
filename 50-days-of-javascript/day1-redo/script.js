a21b2f0fea93758b7c9cca250b61eb1e6e774577
reese <rjkaz@comcast.net>
2022-03-04 13:44:22 -0800

    update

---
+++ 50-days-of-javascript/day1-redo/script.js
@@ -0,0 +1,18 @@
+// bascily we need to switch the active class for the panels.
+
+//first bring in all the panels into the javascript
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
